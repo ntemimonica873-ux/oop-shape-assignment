989e7dba139b1411089d0cf42a8449eba0a6159c
ntemimonica873-ux <ntemimonica873@gmail.com>
2026-07-03 12:13:36 +0300

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle extends Shape {
+    double base;
+    double height;
+
+    public Triangle(String color, double base, double height) {
+        super(color);
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
