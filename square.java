4836ef7447e35a6efc667fbcea3424d2dffa594f
ntemimonica873-ux <ntemimonica873@gmail.com>
2026-07-03 12:14:41 +0300

    Create square.java

---
+++ square.java
@@ -0,0 +1,13 @@
+public class Square extends Shape {
+    double side;
+
+    public Square(String color, double side) {
+        super(color);
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+}
